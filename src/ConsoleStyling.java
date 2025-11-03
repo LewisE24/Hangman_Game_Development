01c6eee71a313e597b00d1786165f3cecd90a2a8
LewisE24 <lellard1@partner.jaguarlandrover.com>
2025-11-03 11:16:11 +0000

    Heavily improved Main Menu Functionality.

--- src/ConsoleStyling.java
+++ src/ConsoleStyling.java
@@ -1,7 +1,7 @@
 public class ConsoleStyling {
     public static final String RESET = "\033[0m";
     public static final String BLACK = "\033[30m";
-    public static final String RED = "\033[31m";
+    public static final String RED = "\033[1;31m";
     public static final String GREEN = "\033[32m";
     public static final String YELLOW = "\033[33m";
     public static final String BLUE = "\033[34m";
@@ -11,20 +11,16 @@ public class ConsoleStyling {
 
     public static String bannerPrint(){
 
-
-
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
 
         return """ 
-            \n ░██     ░██       ░███       ░███    ░██      ░██████     ░███     ░███       ░███       ░███    ░██\s
-             ░██     ░██      ░██░██      ░████   ░██     ░██   ░██    ░████   ░████      ░██░██      ░████   ░██\s
-             ░██     ░██     ░██  ░██     ░██░██  ░██    ░██           ░██░██ ░██░██     ░██  ░██     ░██░██  ░██\s
-             ░██████████    ░█████████    ░██ ░██ ░██    ░██  █████    ░██ ░████ ░██    ░█████████    ░██ ░██ ░██\s
-             ░██     ░██    ░██    ░██    ░██  ░██░██    ░██     ██    ░██  ░██  ░██    ░██    ░██    ░██  ░██░██\s
-             ░██     ░██    ░██    ░██    ░██   ░████     ░██  ░███    ░██       ░██    ░██    ░██    ░██   ░████\s
-             ░██     ░██    ░██    ░██    ░██    ░███      ░█████░█    ░██       ░██    ░██    ░██    ░██    ░███\s
+                  _____ _____ _           ____                          \s
+                 |_   _|_   _| |         / ___| __ _ _ __ ___   ___  ___\s
+                   | |   | | | |   _____| |  _ / _` | '_ ` _ \\ / _ \\/ __|
+                   | |   | | | |__|_____| |_| | (_| | | | | | |  __/\\__ \\
+                   |_|   |_| |_____|     \\____|\\__,_|_| |_| |_|\\___||___/
             """;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,54 +8,109 @@ public class Main {
         Player_Choice();
     }
 
-    public static void Game_Start(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.printf("%nWelcome to Hangman, %s!%n", name);
-        System.out.println("Still " + ConsoleStyling.YELLOW + "WIP" + ConsoleStyling.RESET + "! Press return to return to Main Menu...");
-        Player_Choice();
-    }
-
     public static void Player_Choice() {
         Scanner scanner = new Scanner(System.in);
-        String[] options = {"Play", "Hangman", "Exit"};
+        String[] options = {"MiniGames", "Exit"};
+        boolean running = true;
 
-        // Print out banner
-        System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
-        System.out.println(ConsoleStyling.RESET);
+        while(running) {
+            // Print out banner
+            System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
+            System.out.println(ConsoleStyling.RESET);
 
-        System.out.println("Choose an option: ");
-        for (int i = 0; i < options.length; i++) {
-            System.out.println("\u001B[32m" + (i + 1) + ". " + options[i] + "\u001B[0m");
+            System.out.println("--Main Menu--");
+            for (int i = 0; i < options.length; i++) {
+                System.out.println("\u001B[32m" + (i + 1) + ". " + options[i] + "\u001B[0m");
+            }
+            System.out.print("\nEnter your choice: ");
+
+            int choice = 0;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(ConsoleStyling.RED + "Invalid choice. Make sure to enter a number.");
+                scanner.nextLine();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException r) {
+                    r.printStackTrace();
+                }
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
+                    System.out.println(ConsoleStyling.RESET);
+                    mini_game_menu();
+                    break;
+                case 2:
+                    System.out.println("Goodbye ;)");
+                    running = false;
+                    break;
+                default:
+                    System.out.println(ConsoleStyling.RED + choice + " is not a valid option." + ConsoleStyling.RESET);
+                    try {
+                        Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                    } catch (InterruptedException r) {
+                        r.printStackTrace();
+                    }
+                    System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
+                    System.out.println(ConsoleStyling.RESET);
+                    break;
+            }
         }
+    }
 
 
-        System.out.print("Enter your choice: ");
+    public static void mini_game_menu(){
+        Scanner scanner = new Scanner(System.in);
+        String[] mini_games = {"Hangman", "Return Main Menu"};
+        boolean running = true;
 
-        int choice = 0;
-        try {
-            choice = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println(ConsoleStyling.RED + "Invalid choice. Exiting game");
-            System.exit(1);
-        }
+        while(running) {
+            System.out.println("--MiniGames--");
+            for (int i = 0; i < mini_games.length; i++) {
+                System.out.println("\u001B[32m" + (i + 1) + ". " + mini_games[i] + "\u001B[0m");
+            }
+            System.out.print("\nEnter your choice: ");
 
-        switch (choice) {
-            case 1:
-                System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
-                System.out.println(ConsoleStyling.RESET);
-                Game_Start();
-                break;
-            case 2:
-                Hangman game = new Hangman();
-                game.hangman();
-                Game_Start();
-            case 3:
-                System.out.println("Goodbye ;)");
-                break;
-            default:
-                System.out.println(ConsoleStyling.RED + choice + " is not a valid option.");
+            int choice = 0;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(ConsoleStyling.RED + "Invalid choice. Make sure to enter a number.");
+                scanner.nextLine();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException r) {
+                    e.printStackTrace();
+                }
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
+                    System.out.println(ConsoleStyling.RESET);
+                    Hangman game = new Hangman();
+                    game.hangman();
+                    break;
+                case 2:
+                    System.out.println("Goodbye ;)");
+                    running = false;
+                    break;
+                default:
+                    System.out.println(ConsoleStyling.RED + choice + " is not a valid option." + ConsoleStyling.RESET);
+                    try {
+                        Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                    } catch (InterruptedException r) {
+                        r.printStackTrace();
+                    }
+                    System.out.println(ConsoleStyling.RED + ConsoleStyling.bannerPrint());
+                    System.out.println(ConsoleStyling.RESET);
+                    break;
+            }
         }
     }
 }
